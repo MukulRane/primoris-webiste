c099a6c0245e43c35d9ad9c0125556a0a3b6f2d1
MukulRane <rane.mukul@gmail.com>
2024-06-14 13:48:15 -0700

    mAsset Management page responsive

--- src/pages/AssetManagement/AssetMobile/AssetMobile.js
+++ src/pages/AssetManagement/AssetMobile/AssetMobile.js
@@ -10,8 +10,8 @@ const AssetMobile = () => {
         <p className="case-management-description">
             Mobile application, by using just the standard functions of today’s smart phones, to help you with the time consuming part of the physical asset check. It is, of course, fully integrated with mAsset ERP; this means the download of the asset list and a continuous synchronization with the SAP system. The camera of the phone or tablet could used as a bar code or QR scanner and support of devices equipped with HW barcode / QR code scanners is essential.
         </p>
-        <p>
-        The application mAsset Mobile offers much more than just an easy and effective fixed asset check. You can, for example, create photo documentation or start using GPS coordinates (for property and inventory). All of this additional data is being continuously recorded in SAP together with other results of the physical asset check.
+        <p className="case-management-description">
+          The application mAsset Mobile offers much more than just an easy and effective fixed asset check. You can, for example, create photo documentation or start using GPS coordinates (for property and inventory). All of this additional data is being continuously recorded in SAP together with other results of the physical asset check.
         </p>
       </div>
     </div>

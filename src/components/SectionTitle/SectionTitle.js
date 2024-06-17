aed423c7750d9553a2e673ea1d3039b192ecc135
MukulRane <rane.mukul@gmail.com>
2024-06-17 02:02:54 -0700

    SectionTitle divider fixed

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './SectionTitle.css';
 
 const SectionTitle = ({ title, dividerColor = '#000' }) => {
-  const [inView, setInView] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const sectionTitle = document.querySelector('.section-title');
-      const rect = sectionTitle.getBoundingClientRect();
-      if (rect.top < window.innerHeight) {
-        setInView(true);
-      } else {
-        setInView(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
 
   return (
     <div className="section-title">
       <h2>{title}</h2>
-      <div className={`divider ${inView ? 'in-view' : ''}`} style={{ borderTopColor: dividerColor }}></div>
+      <div className='divider in-view' style={{ borderTopColor: dividerColor }}></div>
     </div>
   );
 };
